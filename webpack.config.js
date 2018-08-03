0c229251fbcd7be29b396b7367313f07c086f3b5
ochi51 <ochiai07@gmail.com>
2018-08-03 20:13:54 +0900

    Fixes webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
   resolve: {
     alias: {
       js: __dirname + '/src'
@@ -18,5 +35,6 @@ module.exports = {
     headers: {
       "Access-Control-Allow-Origin": "*"
     }
-  }
+  },
+  devtool: 'cheap-module-eval-source-map'
 }
